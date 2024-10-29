863979a157c66f15599a8b1845fd44cdd7e843cf
Makhambet Kazybay <makhmarkhov@gmail.com>
2024-10-29 16:50:12 +0500

    Add 28.10 HM

---
+++ HM 28.10/script.js
@@ -0,0 +1,8 @@
+console.log("1) Javascript тілі көмегімен сайтқа интерактивті дүниелерді қосады.")
+
+console.log("2) (Ctrl + /) басу керек")
+
+console.log("3) script ке date деген команда енгыземыз")
+
+console.log("4) External and Internal")
+
