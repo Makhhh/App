e291fc6ab7f45972d1de666eb69de46df3c266cd
Makhambet Kazybay <makhmarkhov@gmail.com>
2024-10-30 16:18:00 +0500

    exercise

---
+++ Exercise/script.js
@@ -0,0 +1,11 @@
+let name = "Makhambet"
+let surname = "Kazybay"
+const birthday = "27.03.2009"
+let school = "152 mektep-gymnazyasi"
+let weight = 57;
+
+console.log(name);
+console.log(surname);
+console.log(birthday);
+console.log(school);
+console.log(weight);
