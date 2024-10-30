6baefaafb829e9621ddd9364e33d599cbe13ec45
Makhambet Kazybay <makhmarkhov@gmail.com>
2024-10-30 16:34:34 +0500

    Exercise 2

---
+++ Exercise2/exercise.js
@@ -0,0 +1,13 @@
+let notebookName = "Acer nitro V15"
+let processor = "Core i5"
+let size = "1920x1080"
+let color = "Standart"
+let todayWeather = "cloudy and cold"
+let curentTime = "16:27"
+
+console.log(notebookName);
+console.log(processor);
+console.log(size);
+console.log(color);
+console.log(todayWeather);
+console.log(curentTime);
